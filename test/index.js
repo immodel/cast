82cca89225f245430e0b86da1349b992ff527ae1
Andrew Shaffer <darawk@gmail.com>
2015-03-07 15:25:02 -0800

    use immodel-base

--- test/index.js
+++ test/index.js
@@ -1,5 +1,6 @@
 var assert = require('assert');
-var model = require('immodel').bootstrap({cast: require('..')});
+var model = require('immodel')
+  .use(require('immodel-base'), {cast: require('..')});
   
 describe('cast', function() {
   it('should work', function() {
