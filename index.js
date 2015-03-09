eafa6e924d62470f244a889df07b116845decda5
Andrew Shaffer <darawk@gmail.com>
2015-03-09 01:23:32 -0700

    immutable data

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
 module.exports = function() {
   this.casters = this.casters || [];
-    
+
   this.caster = function(fn) {
     return this.use(function() {
       this.casters.push(fn);
     });
   }
-     
+
   this.cast = function(value) {
     this.casters.forEach(function(fn) {
       value = fn(value);
     });
-    
+
     return value;
   };
-  
-  this.set(function(value, type) {
-    return type.cast(value);
+
+  this.on('init', function(evt) {
+    evt.doc.value = evt.doc.model.cast(evt.doc.value);
   });
 };
